89ebeb36c6121c4c4a667ada508d743d439b4180
Staglabite <160566168+Staglabite@users.noreply.github.com>
2024-05-15 12:14:16 +0700

    Add files via upload

---
+++ Praktikum 8/Materi 8/ArrayListTest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("Praktikum");
+        strings.add("Collection");
+        strings.add("dan Generics");
+
+        strings.remove("praktikum");
+
+        for(String s : strings){
+            System.out.print(s+" ");
+        }
+    }
+    
+}
